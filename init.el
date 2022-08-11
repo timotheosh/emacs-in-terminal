0ff6e38019fe13bd2bfa00560479e88d02124420
Tim Hawes <trhawes@gmail.com>
2022-08-10 22:20:01 -0400

    Initial commit

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
+(setq package-enable-at-startup nil) ;; Disable loading packages
 (require 'org)
-(require 'org-tempo)
+(require 'org-tempo) ;; Re-enable <s <q <e shortcuts in org-mode
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
